9dbe409188de0ba5fe7b30fc714e44701b52fdb8
cjaewon <jwn4492@outlook.com>
2025-03-12 18:46:39 +0900

    Add github file read feature

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+import os
 import sys
+import tomllib
+from github import Github
+
+def load_config():
+  with open(".env.toml", 'rb') as f:
+    config = tomllib.load(f)
+
+  return config
+
 
 def main():
-    print(sys.argv[0])
+  config = load_config()
+  week = sys.argv[1]
+  
+  g = Github()
+
+  repo = g.get_repo(config["code_review"]["github_repo"])
+
+  for user in config["code_review"]["users"]:
+    files = repo.get_contents(f"{user}/{week}") # 폴더 경로
+
+    for content_file in files:
+      print(f"이름: {content_file.name}, 타입: {content_file.type}")
+
+
+  
 
 
 if __name__ == "__main__":
