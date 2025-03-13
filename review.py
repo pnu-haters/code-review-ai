d8888a61ded15af99b6836c1adfe7be2b0d6f705
cjaewon <jwn4492@outlook.com>
2025-03-13 22:05:59 +0900

    Apply asyncio

--- review.py
+++ review.py
@@ -1,17 +1,17 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 
 class ReviewAI:
   def __init__(self, openai_config):
-    self.client = OpenAI(
+    self.client = AsyncOpenAI(
         api_key=openai_config["api_key"],
     )
     self.prompt = openai_config["prompt"]
 
-  def review(self, boj_problem_text, code):
-    response = self.client.responses.create(
-      model="gpt-4o",
+  async def review(self, boj_problem_text, code, code_type):
+    response = await self.client.responses.create(
+      model="gpt-4o-mini",
       instructions=self.prompt,
       input=f"다음은 내가 풀어야 하는 문제야.\n{boj_problem_text}\n\n다음은 내 코드야.\n{code}",
     )
 
-    return response.output_text
+    return f"""```{code_type}\n{code}\n```\n\n{response.output_text}"""
